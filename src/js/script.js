6adbc178c6bf9933820a1af47a4c0ed2ad63cec3
coulon-estelle <estelle.coulon@edu.univ-fcomte.fr>
2024-12-04 10:52:28 +0100

    refactor lightbox implementation to support dynamic image loading and enhance styling

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,15 @@
 // *** LIGHTBOX ***
-document.getElementById('openDialog').addEventListener('click', function() {
-    document.getElementById('lightbox').showModal();
-});
+const lightbox = document.getElementById('lightbox');
+    const images = document.querySelectorAll('ul img');
 
-document.getElementById('lightbox').addEventListener('click', function() {
-    this.close();
-});
+    images.forEach(img => {
+      img.addEventListener('click', () => {
+        const fullImgSrc = img.dataset.fullImg;
+        lightbox.querySelector('img').src = fullImgSrc;
+        lightbox.showModal();
+    });
+
+    lightbox.addEventListener('click', () => {
+      lightbox.close();
+    });
+    });
