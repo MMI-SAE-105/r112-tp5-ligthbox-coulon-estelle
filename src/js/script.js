c2b17d16c3c519a232dfd501daa88f8b1163dc05
coulon-estelle <estelle.coulon@edu.univ-fcomte.fr>
2024-12-04 10:36:36 +0100

    implement lightbox feature with multiple images and styling

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
 // *** LIGHTBOX ***
+document.getElementById('openDialog').addEventListener('click', function() {
+    document.getElementById('lightbox').showModal();
+});
+
+document.getElementById('lightbox').addEventListener('click', function() {
+    this.close();
+});
